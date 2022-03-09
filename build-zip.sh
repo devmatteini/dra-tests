dd698c0c161f244c057f64919322fd978aa9f268
Cosimo Matteini <dev.matteini@gmail.com>
2022-03-10 00:49:09 +0100

    create bash script to build helloworld.zip

---
+++ build-zip.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+readonly BUILD_DIR="build-zip"
+readonly HELLOWORLD_DIR="$BUILD_DIR/helloworld"
+
+# Create build directory
+if [[ -d "$BUILD_DIR" ]]; then
+    rm -rf "$BUILD_DIR"
+fi
+mkdir -p "$BUILD_DIR"
+
+# Create directory to compress
+mkdir "$HELLOWORLD_DIR"
+cp helloworld/helloworld "$HELLOWORLD_DIR"
+# also add some random files and directories
+touch "$HELLOWORLD_DIR"/LICENSE
+touch "$HELLOWORLD_DIR"/README.md
+mkdir "$HELLOWORLD_DIR"/notes
+touch "$HELLOWORLD_DIR"/notes/latest.txt
+
+# Compress archive
+mkdir -p out/
+(cd "$BUILD_DIR" && zip -r ../out/helloworld.zip .)
+
+
