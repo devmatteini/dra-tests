2b19761598ac75d4c2c722fe996bcba4dbcef60a
Cosimo Matteini <dev.matteini@gmail.com>
2024-05-06 00:22:59 +0200

    support creating pre releases

--- create-release.sh
+++ create-release.sh
@@ -1,36 +1,87 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 function usage(){
-    echo "usage $(basename "$0") <version>" 
+    echo
+    echo "usage $(basename "$0") <VERSION> [--pre-release]"
+    echo
+    echo "OPTIONS:"
+    echo "    [--pre-release]    create a pre-release"
     echo
     echo "ARGS:"
-    echo "    <version>    release version x.y.z (e.g 1.3.0)" 
+    echo "    <VERSION>          release version x.y.z (e.g 1.3.0)"
     exit 1
 }
 
-if [[ -z $1 ]]; then usage; fi
-if [[ $1 == *v* ]]; then echo "Version format must be: x.y.z (e.g 1.3.0)"; usage; fi
-if ! command -v gh > /dev/null; then echo "Missing gh command"; usage; fi
+if [ $# -eq 0 ] || [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+    usage
+fi
 
-version="$1"
-release_notes="dra-tests release $version"
+if ! command -v gh > /dev/null; then 
+    echo "Missing gh command"
+    usage
+fi
 
-git tag -s "$version" -m "$release_notes"
+pre_release="false"
+version=""
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --pre-release)
+            pre_release="true"
+            shift
+            ;;
+        *)
+            if [ -z "$version" ]; then
+                version="$1"
+            fi
+            shift
+            ;;
+    esac
+done
+
+if [ -z "$version" ]; then
+    echo "Error: <VERSION> argument is missing"
+    usage
+fi
+
+if [[ $version == *v* ]]; then 
+    echo "Version format must be: x.y.z (e.g 1.3.0)"
+    usage 
+fi
+
+echo "Version: $version"
+echo "Pre release: $pre_release"
+
+release="$version"
+if [[ "$pre_release" == "true" ]]; then
+    release="$version-alpha"
+fi
+
+release_notes="dra-tests release $release"
+
+git tag -s "$release" -m "$release_notes"
 
 rm -rf out/
 mkdir -p out/
 make build
 
 # Create some assets with version inside filename
-cp out/helloworld.tar.gz out/"helloworld_${version}.tar.gz"
-cp out/helloworld.deb out/"helloworld_${version}_amd64.deb"
+cp out/helloworld.tar.gz out/"helloworld_${release}.tar.gz"
+cp out/helloworld.deb out/"helloworld_${release}_amd64.deb"
 
 git push
 git push --tags
 
+gh_release_create_options=(
+    "$release"
+    --title "$release"
+    --notes "$release_notes"
+    -R devmatteini/dra-tests
+)
+
+if [[ "$pre_release" == "true" ]]; then
+    gh_release_create_options+=("--prerelease")
+fi
+
 # Create release and upload all files inside out/ dir
-gh release create "$version" \
-    --title "$version" \
-    --notes "$release_notes" \
-    -R devmatteini/dra-tests \
-    out/*
+gh release create "${gh_release_create_options[@]}" out/*
