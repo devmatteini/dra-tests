687c732f5b03585cedb0ce957d284273e067c1c3
Cosimo Matteini <dev.matteini@gmail.com>
2024-07-22 22:13:34 +0200

    tar: create archive with executable in bin/ directory

--- create-tar.sh
+++ create-tar.sh
@@ -4,6 +4,7 @@ readonly BUILD_DIR="build-tar"
 readonly HELLOWORLD_DIR="$BUILD_DIR/helloworld"
 readonly HELLOWORLD_WINDOWS_DIR="$BUILD_DIR/helloworld-windows"
 readonly NO_EXECUTABLE_DIR="$BUILD_DIR/no_executable"
+readonly HELLOWORLD_BIN_DIR="$BUILD_DIR/helloworld-bin"
 
 function prepare_helloworld(){
     local dir
@@ -40,6 +41,19 @@ function prepare_no_executable(){
     touch "$dir"/notes/latest.txt
 }
 
+function prepare_helloworld_bin(){
+    local dir
+    dir="$1"
+
+    mkdir "$dir"
+    mkdir "$dir/bin"
+    cp helloworld/helloworld "$dir/bin/helloworld"
+    touch "$dir"/LICENSE
+    touch "$dir"/README.md
+    mkdir "$dir"/notes
+    touch "$dir"/notes/latest.txt
+}
+
 function create_archive_without_root_directory(){
     local dir
     local out_file
@@ -58,6 +72,7 @@ mkdir -p "$BUILD_DIR"
 prepare_helloworld "$HELLOWORLD_DIR"
 prepare_no_executable "$NO_EXECUTABLE_DIR"
 prepare_helloworld_windows "$HELLOWORLD_WINDOWS_DIR"
+prepare_helloworld_bin "$HELLOWORLD_BIN_DIR"
 
 # Compress archives
 mkdir -p out/
@@ -71,6 +86,7 @@ tar czf out/no_executable.tar.gz -C "$BUILD_DIR" no_executable/
 tar cjf out/helloworld.tar.bz2 -C "$BUILD_DIR" helloworld/ 
 tar cJf out/helloworld.tar.xz -C "$BUILD_DIR" helloworld/ 
 tar czf out/helloworld-x86_64-linux.tar.gz -C "$BUILD_DIR" helloworld/
+tar czf out/helloworld-bin-x86_64-linux.tar.gz -C "$BUILD_DIR" helloworld-bin/
 
 # Windows
 tar czf out/helloworld-windows.tar.gz -C "$BUILD_DIR" helloworld-windows/ 
